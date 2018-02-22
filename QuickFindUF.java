dcd8046256b4ace00761c01ef01eafb756b88160
Maddie <kirwinma@grinnell.edu>
2018-02-23 00:25:04 +0100

    added QuickUnion-ish methods

--- QuickFindUF.java
+++ QuickFindUF.java
@@ -13,6 +13,9 @@ public class QuickFindUF {
         return id[p] == id[q];
     }
 
+    /** Simply changes the connectivity group to that of the 'second'
+     * For a more efficient algorithm, make "roots"
+     */
     public void union(int p, int q) {
         int firstGroup = id[p];
         int secondGroup = id[q];
@@ -24,6 +27,31 @@ public class QuickFindUF {
                 }
             }
         }
+    }
+
+    /** Makes a hierarchical tree structure instead.
+     * quickUnion is slow in a different way: you could have a worst-case
+     * tree that is basically a linked list, making the connected method 
+     * terrible.
+     */
+    public void quickUnion(int p, int q) {
+        int i = id[p];
+        int j = id[q];
+        id[i] = j;
+    }
+
+    /** Despite its name, it is not quick.
+     * This is due to the shitty-ish nature of quickUnion.
+     */
+    public quickConnected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    /**Finds the root of an item (at index i).
+     */
+    private root(int i) {
+        while (i != id[i]) { i = id[i]; }
+        return i;
     }
 
 }
