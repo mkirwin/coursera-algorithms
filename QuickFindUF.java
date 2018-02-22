a872f48665586d28fd489a023fd2d564d5aa75c9
Maddie <kirwinma@grinnell.edu>
2018-02-23 00:05:52 +0100

    naive QuickFindUF implementation

---
+++ QuickFindUF.java
@@ -0,0 +1,29 @@
+public class QuickFindUF {
+
+    private int[] id;
+
+    public QuickFindUF(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    public boolean connected(int p, int q) {
+        return id[p] == id[q];
+    }
+
+    public void union(int p, int q) {
+        int firstGroup = id[p];
+        int secondGroup = id[q];
+
+        if (firstGroup != secondGroup) {
+            for (int i = 0; i < id.length; i++) {
+                if (id[i] == firstGroup) {
+                    id[i] = secondGroup;
+                }
+            }
+        }
+    }
+
+}
